c1d5f1e7f9724b8bb3d90fb4de8b1a3983316e35
Pooja G <poojagajendran91@gmail.com>
2025-12-09 11:15:06 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,10 @@
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favourite hobby? ")
+city = input("Which city do you live in? ")
+
+
+print("\n🎉 Welcome", name + "! 🎉")
+print(f"You are {age} years old, love {hobby}, and you live in {city}.")
+print("Glad to have you here! 😊")
